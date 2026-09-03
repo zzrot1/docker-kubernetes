f2468f6cbb415861b5c9dc19b461723b6b539f9c
imihu <imihu97@gmail.com>
2026-09-03 22:44:15 +0300

    s13 - added the s13 code that I belive in god knows what that legacy hello world !lorem ipsum o(tripple h) works trippaloski addidas kasoski vodka plm 17 pro max code

--- section-12-key-value-app/backend/src/server.js
+++ section-12-key-value-app/backend/src/server.js
@@ -1,23 +1,29 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
-app.get("/health", (req,res) => {
-    res.status(200).send("up!!")
-})
+app.get("/health", (req, res) => {
+  res.status(200).send("up!!!!");
+});
 
-mongoose.connect(`mongodb://${process.env.MONGODB_HOST}/${process.env.KEY_VALUE_DB}`, {
-    auth: {
+mongoose
+  .connect(
+    `mongodb://${process.env.MONGODB_HOST}/${process.env.KEY_VALUE_DB}`,
+    {
+      auth: {
         username: process.env.KEY_VALUE_USER,
-        password: process.env.KEY_VALUE_PASSWORD
+        password: process.env.KEY_VALUE_PASSWORD,
+      },
+      connectTimeoutMS: 500,
     },
-    connectTimeoutMS: 500
-})
-.then(() => {
-    app.listen(process.env.PORT, ()=> {console.log(`Listening to port ${process.env.PORT}`)})
-    console.log("Connected to DB")
-})
-.catch(err=> console.log(err))
+  )
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening to port ${process.env.PORT}`);
+    });
+    console.log("Connected to DB");
+  })
+  .catch((err) => console.log(err));
